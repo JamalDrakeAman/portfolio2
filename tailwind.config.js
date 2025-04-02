de308ed03ef9a27594ea60089ca8810dfd19752c
Jamal Drake Aman <jamaldrakeaman@yahoo.com>
2025-04-02 14:59:24 +0200

    style and responsive hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
       backgroundImage:{
         'hero-bg': "url('/assets/design/bg-hero.png')",
         'profil-img': "url('/assets/imgs/profil-img.jpg')"
-      }
+      },
+      screens: {
+        '3xl': '1920px',  // Custom Breakpoint für 4K/Full HD
+        '4xl': '2560px',   // Ultra-Wide/QHD
+      },
     },
   },
   plugins: [],
