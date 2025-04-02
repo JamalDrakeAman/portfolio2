0bffbb87297b4b7a8ac9badcaa4c6c977f3acafa
Jamal Drake Aman <jamaldrakeaman@yahoo.com>
2025-04-02 17:25:47 +0200

    style and responsive about section

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,10 @@ module.exports = {
     extend: {
       backgroundImage:{
         'hero-bg': "url('/assets/design/bg-hero.png')",
-        'profil-img': "url('/assets/imgs/profil-img.jpg')"
+        'profil-img': "url('/assets/imgs/profil-img.jpg')",
+        'about-label-1': "url('/assets/design/label1.svg')",
+        'about-label-2': "url('/assets/design/label2.svg')",
+        'about-label-3': "url('/assets/design/label3.svg')"
       },
       screens: {
         '3xl': '1920px',  // Custom Breakpoint für 4K/Full HD
